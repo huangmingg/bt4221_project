5de8c2db0c77c1e9b904c7f53b0f77d8b8937ec1
huangmingg <68838289+huangmingg@users.noreply.github.com>
2021-11-20 11:25:46 +0800

    Updates

--- src/metrics.py
+++ src/metrics.py
@@ -1,5 +1,6 @@
-from sklearn.metrics import roc_curve, top_k_accuracy_score, balanced_accuracy_score
+from sklearn.metrics import roc_curve, balanced_accuracy_score, average_precision_score, roc_auc_score, accuracy_score
 from tensorflow.keras.callbacks import History
+from typing import List
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -39,5 +40,18 @@ def plot_roc(name: str, labels: np.array, predictions: np.array, **kwargs):
     ax.set_aspect('equal')
 
 
-def compute_score(labels: np.array, prediction: np.array):
-    pass
+def compute_score(name: str, labels: np.array, pred: np.array) -> None:
+    label_1d = np.array(list(map(lambda x: list(x).index(max(x)), labels)))
+    pred_1d = np.array(list(map(lambda x: list(x).index(max(x)), pred)))
+    print(f"({name}) ROC: {roc_auc_score(labels, pred)}, AUPRC: { average_precision_score(labels, pred)}")
+    print(f"({name}) Accuracy: {accuracy_score(label_1d, pred_1d)} Balanced Accuracy: {balanced_accuracy_score(label_1d, pred_1d)}")
+    
+
+
+def one_hot_encode_labels(labels: List[int]) -> np.array:
+    class_no = len(np.unique(np.array(labels)))
+    output = np.zeros((len(labels), class_no))
+    for idx, v in enumerate(labels):
+      output[idx][v] = 1.0
+    return output
+
